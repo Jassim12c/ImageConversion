f337c6f5adb3cd923e67daa5cc9462b03ec96e7d
Jassim Alhatem <duod2003row@gmail.com>
2020-11-27 16:10:41 +0300

    added a banner for the page

--- main.py
+++ main.py
@@ -11,14 +11,32 @@ class ImageConv:
 
         #  Calling methods
         self.build_grid()
+        self.build_banner()
 
     def build_grid(self):
         self.mainframe.columnconfigure(0, weight=1)
         self.mainframe.columnconfigure(1, weight=1)
+        self.mainframe.columnconfigure(2, weight=0)
         self.mainframe.rowconfigure(0, weight=0)
         self.mainframe.rowconfigure(1, weight=1)
+
+    def build_banner(self):
+        banner = tkinter.Label(
+            self.mainframe,
+            text="Image Conversion",
+            bg="blue",
+            fg="black",
+            font=("Courier", 24)
+        )
+        banner.grid(
+            row=0, column=1,
+            sticky="ew",
+            padx=10, pady=10,
+        )
 
 if __name__ == "__main__":
     root = tkinter.Tk()
     ImageConv(root)
     root.mainloop()
+
+
